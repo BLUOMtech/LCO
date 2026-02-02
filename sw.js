ec66e0f5b7a94a88e0eedaf72fd6c725bdb9c083
Blade <bladepeisker82@gmail.com>
2026-02-02 16:00:28 +1000

    Log message on service worker installation
    Added console log for service worker installation.

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
 self.addEventListener("install", e => {
   self.skipWaiting();
+  console.log("Service Worker installed");
+});
+
+self.addEventListener("fetch", e => {
+  // optional: add caching here
 });
